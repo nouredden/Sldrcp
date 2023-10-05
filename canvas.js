abe5d7b777c19b0ff82140152af3b8a7afb9a925
nour edden <noureddenrz@outlook.com>
2023-10-05 09:42:56 +0800

    done

---
+++ canvas.js
@@ -0,0 +1,22 @@
+function doColor() {
+    var canvas = document.getElementById("can");
+    var colorinput = document.getElementById("clr");
+    canvas.style.backgroundColor = colorinput.value;
+  }
+  
+  function doSquare() {
+    var sliderinput = document.getElementById("slr");
+    var len = sliderinput.value;
+    var canvas = document.getElementById("can");
+    var context = canvas.getContext("2d");
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.fillStyle = "yellow";
+    context.fillRect(5,5,len,len);
+    context.fillRect(parseInt(len)+25,10,len,len);
+    context.fillRect(len*3,10,len,len);
+  }
+  
+  
+  
+  
+  
